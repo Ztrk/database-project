f020feea63811dc52124f1941308dcdf32eb7001
Marcin Zatorski <mar3211233@gmail.com>
2020-01-22 20:55:08 +0100

    Add some test data

---
+++ sql/test_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO obiekt_astronomiczny(nazwa) VALUES
+('Venus'),
+('Jupiter'),
+('Sun'),
+('Milky Way');
+
+INSERT INTO konstelacja(skrot_iau, nazwa, najjasniejsza_gwiazda) VALUES
+('Sgr', 'Sagittarius', 'Epsilon Sagittarii'),
+('Sct', 'Scutum', 'Alpha Scuti');
