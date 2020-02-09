857eaa8141f08a6cc44870450c15ab1b628508da
Marcin Zatorski <mar3211233@gmail.com>
2020-02-09 23:17:20 +0100

    Add indexes to database

---
+++ sql/indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX constellation_name_index ON konstelacja(nazwa);
+
+CREATE INDEX observatory_name_index ON obserwatorium(pelna_nazwa);
+
+CREATE INDEX catalogue_abbreviation_index ON katalog(skrot);
