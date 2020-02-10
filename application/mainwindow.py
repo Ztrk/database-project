b7c27882e4a902c3f92bdedda80ff457b2b2f985
MarcinOdor <marcin31313@gmail.com>
2020-02-10 17:27:13 +0100

    Added procedure changing gregorian dates to julian days and vice versa
    At the moment it's impossible to add or edit existing row while they are
    using Julian dates. Futhermore, the procedure doesn't affect the observations'
    table.

--- application/mainwindow.py
+++ application/mainwindow.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtWidgets, uic
+import sqlalchemy as db
 import astronomy
 import astronomy_model as model
 import forms
@@ -18,7 +19,23 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.action_add.triggered.connect(self.add_to_table)
         self.action_remove.triggered.connect(self.remove_from_table)
-        self.action_edit.triggered.connect(self.edit_table)
+        self.action_edit.triggered.connect(lambda: self.edit_table(self.action_date_to_JD.isChecked()))
+        self.action_date_to_JD.toggled.connect(lambda: self.jd_to_date(self.action_date_to_JD.isChecked()))
+        # print(User.query.filter_by(username='admin').first())
+
+    def closeEvent(self, event):
+        if self.action_date_to_JD.isChecked():
+            try:
+                result = self.session.execute("CALL AllJDToDate();")
+                result.close()
+                astronomy.Astronomer.death_date.property.columns[0].type = db.Date()
+                astronomy.Astronomer.birth_date.property.columns[0].type = db.Date()
+                # astronomy.Astronomer.birth_date = db.Column('data_urodzenia', db.Date)
+                # astronomy.Astronomer.death_date = db.Column('data_zgonu', db.Date)
+                self.session.commit()
+            except:
+                self.session.rollback()
+                raise
 
     def fill_table_selector(self):
         objects = TableSelectorItem(self.table_selector, 'Obiekty astronomiczne')
@@ -35,7 +52,7 @@ class MainWindow(QtWidgets.QMainWindow):
         TableSelectorItem(self.table_selector, 'Obserwatoria', self.session, model.ObservatoryModel, forms.ObservatoryForm)
         TableSelectorItem(self.table_selector, 'Astronomowie', self.session, model.AstronomerModel, forms.AstronomerForm)
 
-    def item_changed_handler(self, current, previous):
+    def item_changed_handler(self, current):
         if current.model is not None:
             self.table_view.setModel(current.model)
             self.current_model = current.model
@@ -51,11 +68,48 @@ class MainWindow(QtWidgets.QMainWindow):
             if selected_row >= 0:
                 self.current_form.remove_row(selected_row, self)
 
-    def edit_table(self):
+    def edit_table(self, isJD):
         if self.current_form is not None:
             selected_row = self.table_view.currentIndex().row()
             if selected_row >= 0:
+                # if self.current_form.model.type.birth_date.property.columns[0].type == db.Date and isJD:
+                #     self.current_form.model.type.birth_date.property.columns[0].type = db.Numeric()
+                #     self.current_form.model.type.death_date.property.columns[0].type = db.Numeric()
+                # print(self.current_form.dialog)
                 self.current_form.edit_row(selected_row, self)
+
+    def jd_to_date(self, isJD):
+        if isJD:
+            try:
+                result = self.session.execute('CALL AllDateToJD();')
+                result.close()
+                self.session.commit()
+                astronomy.Astronomer.death_date.property.columns[0].type = db.Numeric()
+                astronomy.Astronomer.birth_date.property.columns[0].type = db.Numeric()
+                # astronomy.Astronomer.birth_date = db.Column('data_urodzenia', db.Numeric)
+                # astronomy.Astronomer.death_date = db.Column('data_zgonu', db.Numeric)
+                self.update()
+                # astronomy.Astronomer.birth_date.property.columns[0].type = db.Numeric()
+                # astronomy.Astronomer.death_date.property.columns[0].type = db.Numeric()
+                # print(astronomy.Galaxy.diameter.property.columns)
+                # print(astronomy.Astronomer.death_date.name)
+            except:
+                self.session.rollback()
+                raise
+        else:
+            try:
+                result = self.session.execute("CALL AllJDToDate();")
+                result.close()
+                self.session.commit()
+                astronomy.Astronomer.death_date.property.columns[0].type = db.Date()
+                astronomy.Astronomer.birth_date.property.columns[0].type = db.Date()
+                # astronomy.Astronomer.birth_date = db.Column('data_urodzenia', db.Date)
+                # astronomy.Astronomer.death_date = db.Column('data_zgonu', db.Date)
+                self.update()
+                # print(astronomy.Astronomer.death_date.property.columns[0].type)
+            except:
+                self.session.rollback()
+                raise
 
 
 class TableSelectorItem(QtWidgets.QTreeWidgetItem):
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -59,8 +59,8 @@ ALTER TABLE obiekt_astronomiczny ADD CONSTRAINT obiekt_astronomiczny_pk PRIMARY 
 
 CREATE TABLE grupa_galaktyk (
     nazwa               VARCHAR(70) NOT NULL,
-    rektasencja         DECIMAL(10, 6), 
-    deklinacja          DECIMAL(10, 6), 
+    rektasencja         DECIMAL(10, 6),
+    deklinacja          DECIMAL(10, 6),
     dystans             DECIMAL(10, 4),
     predkosc_radialna   DECIMAL(10, 4)
 );
@@ -79,11 +79,11 @@ CHECK (deklinacja >= -90 AND deklinacja <= 90);
 CREATE TABLE galaktyka (
     nazwa                  VARCHAR(70) NOT NULL,
     typ                    VARCHAR(20) NOT NULL,
-    rektasencja            DECIMAL(10, 6), 
-    deklinacja             DECIMAL(10, 6), 
-    wielkosc_obserwowana   DECIMAL(6, 4), 
-    wielkosc_absolutna     DECIMAL(6, 4) NOT NULL, 
-    dystans                DECIMAL(10, 4), 
+    rektasencja            DECIMAL(10, 6),
+    deklinacja             DECIMAL(10, 6),
+    wielkosc_obserwowana   DECIMAL(6, 4),
+    wielkosc_absolutna     DECIMAL(6, 4) NOT NULL,
+    dystans                DECIMAL(10, 4),
     srednica               DECIMAL(10, 4) NOT NULL,
     grupa_galaktyk         VARCHAR(70),
     orbitowana_galaktyka   VARCHAR(70),
@@ -92,7 +92,7 @@ CREATE TABLE galaktyka (
 
 ALTER TABLE galaktyka ADD CONSTRAINT galaktyka_pk PRIMARY KEY (nazwa);
 
-ALTER TABLE galaktyka ADD CONSTRAINT galaktyka_obiekt_astronomiczny_fk 
+ALTER TABLE galaktyka ADD CONSTRAINT galaktyka_obiekt_astronomiczny_fk
 FOREIGN KEY (nazwa) REFERENCES obiekt_astronomiczny (nazwa);
 
 ALTER TABLE galaktyka ADD CONSTRAINT galaktyka_grupa_galaktyk_fk
@@ -115,11 +115,11 @@ CREATE TABLE gwiazda (
     typ_widmowy            VARCHAR(20) NOT NULL,
     rektasencja            DECIMAL(10, 6),
     deklinacja             DECIMAL(10, 6),
-    wielkosc_obserwowana   DECIMAL(6, 4) NOT NULL, 
-    wielkosc_absolutna     DECIMAL(6, 4) NOT NULL, 
-    dystans                DECIMAL(10, 4) NOT NULL, 
-    paralaksa              DECIMAL(10, 4), 
-    masa                   DECIMAL(10, 4) NOT NULL, 
+    wielkosc_obserwowana   DECIMAL(6, 4) NOT NULL,
+    wielkosc_absolutna     DECIMAL(6, 4) NOT NULL,
+    dystans                DECIMAL(10, 4) NOT NULL,
+    paralaksa              DECIMAL(10, 4),
+    masa                   DECIMAL(10, 4) NOT NULL,
     promien                DECIMAL(10, 4) NOT NULL,
     galaktyka              VARCHAR(70) NOT NULL,
     konstelacja            VARCHAR(8)
@@ -145,15 +145,15 @@ CHECK (deklinacja >= -90 AND deklinacja <= 90);
 CREATE TABLE male_cialo (
     nazwa                        VARCHAR(70) NOT NULL,
     typ                          VARCHAR(30),
-    okres                        DECIMAL(10, 4), 
-    ekscentrycznosc              DECIMAL(8, 6) NOT NULL, 
-    polos_wielka                 DECIMAL(10, 4) NOT NULL, 
-    inklinacja                   DECIMAL(7, 4) NOT NULL, 
-    dlugosc_wezla_wstepujacego   DECIMAL(7, 4) NOT NULL, 
-    argument_perycentrum         DECIMAL(7, 4) NOT NULL, 
-    anomalia_srednia             DECIMAL(7, 4) NOT NULL, 
-    epoch                        DECIMAL(10, 4) NOT NULL, 
-    srednica                     DECIMAL(10, 4) NOT NULL, 
+    okres                        DECIMAL(10, 4),
+    ekscentrycznosc              DECIMAL(8, 6) NOT NULL,
+    polos_wielka                 DECIMAL(10, 4) NOT NULL,
+    inklinacja                   DECIMAL(7, 4) NOT NULL,
+    dlugosc_wezla_wstepujacego   DECIMAL(7, 4) NOT NULL,
+    argument_perycentrum         DECIMAL(7, 4) NOT NULL,
+    anomalia_srednia             DECIMAL(7, 4) NOT NULL,
+    epoch                        DECIMAL(10, 4) NOT NULL,
+    srednica                     DECIMAL(10, 4) NOT NULL,
     masa                         DECIMAL(10, 4),
     orbitowana_gwiazda           VARCHAR(70),
     orbitowane_male_cialo        VARCHAR(70),
@@ -178,9 +178,9 @@ CHECK ((orbitowane_male_cialo IS NOT NULL AND orbitowana_gwiazda IS NULL)
 
 CREATE TABLE sztuczny_satelita (
     nazwa                   VARCHAR(70) NOT NULL,
-    okres_orbitalny         DECIMAL(10, 4), 
-    apocentrum              DECIMAL(10, 4) NOT NULL, 
-    perycentrum             DECIMAL(10, 4) NOT NULL, 
+    okres_orbitalny         DECIMAL(10, 4),
+    apocentrum              DECIMAL(10, 4) NOT NULL,
+    perycentrum             DECIMAL(10, 4) NOT NULL,
     inklinacja              DECIMAL(7, 4) NOT NULL,
     data_startu             DATE NOT NULL,
     data_zniszczenia        DATE,
@@ -211,7 +211,7 @@ CREATE TABLE roj_meteorow (
     data_poczatku   DATE NOT NULL,
     data_końca      DATE NOT NULL,
     data_maksimum   DATE NOT NULL,
-    rektasencja     DECIMAL(10, 6) NOT NULL, 
+    rektasencja     DECIMAL(10, 6) NOT NULL,
     deklinacja      DECIMAL(10, 6) NOT NULL,
     predkosc        DECIMAL(5, 2) NOT NULL,
     zhr             DECIMAL(6, 3) NOT NULL,
@@ -242,7 +242,7 @@ CREATE TABLE obiekt_w_katalogu (
     katalog_nazwa   VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE obiekt_w_katalogu ADD CONSTRAINT obiekt_w_katalogu_pk 
+ALTER TABLE obiekt_w_katalogu ADD CONSTRAINT obiekt_w_katalogu_pk
 PRIMARY KEY (obiekt_nazwa, katalog_nazwa);
 
 ALTER TABLE obiekt_w_katalogu ADD CONSTRAINT obiekt_w_katalogu_katalog_fk
@@ -271,3 +271,142 @@ FOREIGN KEY (obserwatorium) REFERENCES obserwatorium (kod_iau);
 ALTER TABLE obserwacja ADD CONSTRAINT obserwacja_astronom_fk
 FOREIGN KEY (astronom) REFERENCES astronom (pelne_imie);
 
+-- Functions and procedures
+DROP FUNCTION IF EXISTS JDToDate;
+DROP FUNCTION IF EXISTS DateToJD;
+DROP PROCEDURE IF EXISTS AllJDToDate;
+DROP PROCEDURE IF EXISTS AllDateToJD;
+DELIMITER $$
+CREATE FUNCTION JDToDate(pJD NUMERIC)
+    RETURNS DATE
+    -- RETURNS VARCHAR(10)
+BEGIN
+    DECLARE X NUMERIC; DECLARE Y NUMERIC; DECLARE A NUMERIC; DECLARE B NUMERIC;
+    DECLARE C NUMERIC; DECLARE E NUMERIC; DECLARE F NUMERIC; DECLARE R NUMERIC;
+    DECLARE M NUMERIC; DECLARE D NUMERIC;
+    DECLARE vData VARCHAR(10); DECLARE charR VARCHAR(4);
+    DECLARE charM VARCHAR(2); DECLARE charD VARCHAR(2);
+    SET X = FLOOR(pJD + 0.5);
+    SET Y = FLOOR((X + 32044.5) / 36524.25);
+    SET A = X + Y - FLOOR(Y / 4) - 38 + 1524;
+    SET B = FLOOR ((A - 122.1) / 365.25);
+    SET C = A - FLOOR (365.25 * B);
+    SET E = FLOOR (C / 30.61);
+    SET F = FLOOR (E / 14);
+    SET R = B - 4716 + F;
+    SET M = E - 1 - 12 * F;
+    SET D = C + pJD + 0.5 - X - FLOOR (153 * E / 5) - 1;
+    SET charR = CONVERT(R, CHAR);
+    WHILE CHAR_LENGTH(charR) != 4 DO
+        SET charR = CONCAT('0', charR);
+    END WHILE;
+    SET charM = CONVERT(M, CHAR);
+    WHILE CHAR_LENGTH(charM) != 2 DO
+        SET charM = CONCAT('0', charM);
+    END WHILE;
+    SET charD = CONVERT(D, CHAR);
+    WHILE CHAR_LENGTH(charD) != 2 DO
+        SET charD = CONCAT('0', charD);
+    END WHILE;
+    SET vData = CONCAT(charR, '-', charM, '-', charD);
+    -- RETURN vData;
+    RETURN (CONVERT(vData, DATE));
+END$$
+
+CREATE FUNCTION DateToJD(pDate DATE)
+    RETURNS NUMERIC
+BEGIN
+    DECLARE A NUMERIC;  DECLARE B NUMERIC; DECLARE E NUMERIC;
+    DECLARE JD NUMERIC;
+    DECLARE R INTEGER; DECLARE M INTEGER; DECLARE D INTEGER;
+    SET R = YEAR(pDate);
+    SET M = MONTH(pDate);
+    SET D = DAY(pDate);
+    SET A = 4716 + R + FLOOR((M + 9) / 12);
+    SET B = 1729279.5 + 367 * R + FLOOR(275 * M / 9) - FLOOR(7 * A / 4) + D;
+    SET E = FLOOR(3 * (FLOOR((A + 83) / 100) + 1) / 4);
+    SET JD = B + 38 - E;
+    RETURN JD;
+END$$
+
+CREATE PROCEDURE AllDateToJD()
+BEGIN
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT pelne_imie,data_urodzenia,data_zgonu FROM astronom);
+    ALTER TABLE astronom
+        MODIFY COLUMN data_zgonu NUMERIC,
+        MODIFY COLUMN data_urodzenia NUMERIC;
+    UPDATE astronom t1 INNER JOIN tempDataInfo t2 ON t1.pelne_imie = t2.pelne_imie SET t1.data_urodzenia = DateToJD(t2.data_urodzenia);
+    UPDATE astronom t1 INNER JOIN tempDataInfo t2 ON t1.pelne_imie = t2.pelne_imie SET t1.data_zgonu = DateToJD(t2.data_zgonu);
+    DROP TEMPORARY TABLE tempDataInfo;
+
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT nazwa,data_startu,data_zniszczenia FROM sztuczny_satelita);
+    ALTER TABLE sztuczny_satelita
+        MODIFY COLUMN data_startu NUMERIC,
+        MODIFY COLUMN data_zniszczenia NUMERIC;
+    UPDATE sztuczny_satelita t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_startu = DateToJD(t2.data_startu);
+    UPDATE sztuczny_satelita t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_zniszczenia = DateToJD(t2.data_zniszczenia);
+    DROP TEMPORARY TABLE tempDataInfo;
+
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT nazwa,data_poczatku,data_końca,data_maksimum FROM roj_meteorow);
+    ALTER TABLE roj_meteorow
+        MODIFY COLUMN data_poczatku NUMERIC,
+        MODIFY COLUMN data_końca NUMERIC,
+        MODIFY COLUMN data_maksimum NUMERIC;
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_poczatku = DateToJD(t2.data_poczatku);
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_końca = DateToJD(t2.data_końca);
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_maksimum = DateToJD(t2.data_maksimum);
+    DROP TEMPORARY TABLE tempDataInfo;
+    --
+    -- CREATE TEMPORARY TABLE tempDataInfo AS (SELECT data,obiekt_astronomiczny,obserwatorium,astronom FROM obserwacja);
+    -- ALTER TABLE obserwacja
+    --     MODIFY COLUMN data NUMERIC;
+    -- UPDATE obserwacja t1 INNER JOIN tempDataInfo t2 ON (t1.data = t2.data AND t1.obiekt_astronomiczny = t2.obiekt_astronomiczny AND
+    -- t1.obserwatorium = t2.obserwatorium AND t1.astronom = t2.astronom) SET t1.data = DateToJD(t2.data);
+    -- DROP TEMPORARY TABLE tempDataInfo;
+END$$
+
+CREATE PROCEDURE AllJDToDate()
+BEGIN
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT pelne_imie,data_urodzenia,data_zgonu FROM astronom);
+    UPDATE astronom SET data_urodzenia = 19990101 WHERE data_urodzenia IS NOT NULL;
+    UPDATE astronom SET data_zgonu = 19990101 WHERE data_zgonu IS NOT NULL;
+    ALTER TABLE astronom
+        MODIFY COLUMN data_zgonu DATE,
+        MODIFY COLUMN data_urodzenia DATE;
+    UPDATE astronom t1 INNER JOIN tempDataInfo t2 ON t1.pelne_imie = t2.pelne_imie SET t1.data_urodzenia = JDToDate(t2.data_urodzenia);
+    UPDATE astronom t1 INNER JOIN tempDataInfo t2 ON t1.pelne_imie = t2.pelne_imie SET t1.data_zgonu = JDToDate(t2.data_zgonu);
+    DROP TEMPORARY TABLE tempDataInfo;
+
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT nazwa,data_startu,data_zniszczenia FROM sztuczny_satelita);
+    UPDATE sztuczny_satelita SET data_startu = 19990101 WHERE data_startu IS NOT NULL;
+    UPDATE sztuczny_satelita SET data_zniszczenia = 19990101 WHERE data_zniszczenia IS NOT NULL;
+    ALTER TABLE sztuczny_satelita
+        MODIFY COLUMN data_startu DATE,
+        MODIFY COLUMN data_zniszczenia DATE;
+    UPDATE sztuczny_satelita t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_startu = JDToDate(t2.data_startu);
+    UPDATE sztuczny_satelita t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_zniszczenia = JDToDate(t2.data_zniszczenia);
+    DROP TEMPORARY TABLE tempDataInfo;
+
+    CREATE TEMPORARY TABLE tempDataInfo AS (SELECT nazwa,data_poczatku,data_końca,data_maksimum FROM roj_meteorow);
+    UPDATE roj_meteorow SET data_poczatku = 19990101 WHERE data_poczatku IS NOT NULL;
+    UPDATE roj_meteorow SET data_końca = 19990101 WHERE data_końca IS NOT NULL;
+    UPDATE roj_meteorow SET data_maksimum = 19990101 WHERE data_maksimum IS NOT NULL;
+    ALTER TABLE roj_meteorow
+        MODIFY COLUMN data_poczatku DATE,
+        MODIFY COLUMN data_końca DATE,
+        MODIFY COLUMN data_maksimum DATE;
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_poczatku = JDToDate(t2.data_poczatku);
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_końca = JDToDate(t2.data_końca);
+    UPDATE roj_meteorow t1 INNER JOIN tempDataInfo t2 ON t1.nazwa = t2.nazwa SET t1.data_maksimum = JDToDate(t2.data_maksimum);
+    DROP TEMPORARY TABLE tempDataInfo;
+    --
+    -- CREATE TEMPORARY TABLE tempDataInfo AS (SELECT data,obiekt_astronomiczny,obserwatorium,astronom FROM obserwacja);
+    -- UPDATE obserwacja t1 INNER JOIN tempDataInfo t2 ON (t1.data = t2.data AND t1.obiekt_astronomiczny = t2.obiekt_astronomiczny AND
+    -- t1.obserwatorium = t2.obserwatorium AND t1.astronom = t2.astronom) SET t1.data = JDToDate(t2.data);
+    -- ALTER TABLE obserwacja
+    --     MODIFY COLUMN data DATE;
+    -- UPDATE obserwacja t1 INNER JOIN tempDataInfo t2 ON (t1.data = t2.data AND t1.obiekt_astronomiczny = t2.obiekt_astronomiczny AND
+    -- t1.obserwatorium = t2.obserwatorium AND t1.astronom = t2.astronom) SET t1.data = JDToDate(t2.data);
+    -- DROP TEMPORARY TABLE tempDataInfo;
+END$$
+DELIMITER ;
